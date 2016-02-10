11770cffe2fd47c05a9d2b98ac70bcd7650c2a33
Morten Møller Jakobsen <treeline@live.dk>
2016-02-10 11:19:59 +0100

    GPS service giver nu regulære opdateringer

--- LBForsikring/app/src/main/java/sw10/lbforsikring/LocationListener.java
+++ LBForsikring/app/src/main/java/sw10/lbforsikring/LocationListener.java
@@ -1,35 +1,20 @@
 package sw10.lbforsikring;
 
 import android.location.Location;
-import android.os.Bundle;
 import android.util.Log;
 
 /**
  * Created by treel on 09-02-2016.
  */
-    public class LocationListener implements android.location.LocationListener {
+    public class LocationListener implements com.google.android.gms.location.LocationListener {
         Location mLastLocation;
 
-        public LocationListener(String provider) {
-            mLastLocation = new Location(provider);
-
+        public LocationListener() {
         }
 
         @Override
         public void onLocationChanged(Location location) {
-            mLastLocation.set(location);
+            mLastLocation = location;
             Log.e("App", "Lat: " + location.getLatitude() + ", Lng: " + location.getLongitude());
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
         }
 }
--- LBForsikring/app/src/main/java/sw10/lbforsikring/MainActivity.java
+++ LBForsikring/app/src/main/java/sw10/lbforsikring/MainActivity.java
@@ -3,7 +3,9 @@ package sw10.lbforsikring;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +28,8 @@ public class MainActivity extends Activity {
         public void onClick(View v) {
             Button toggleDrivingButton = (Button)findViewById(R.id.toggleDrivingButton);
 
+            //If user was not driving, start the service, otherwise stop it.
+            //Change text on button accordingly
             if (!mDriving) {
                 startService(new Intent(mContext, LocationService.class));
                 Log.e("App", "Service Started");
