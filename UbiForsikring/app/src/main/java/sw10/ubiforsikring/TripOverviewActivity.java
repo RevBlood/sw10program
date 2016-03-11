62f432d119fb32aaa3b1bb2b07eab9ccb3f537af
Morten Møller Jakobsen <treeline@live.dk>
2016-03-11 15:08:38 +0100

    Now with graphs

--- UbiForsikring/app/src/main/java/sw10/ubiforsikring/TripOverviewActivity.java
+++ UbiForsikring/app/src/main/java/sw10/ubiforsikring/TripOverviewActivity.java
@@ -3,10 +3,24 @@ package sw10.ubiforsikring;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TripOverviewActivity extends AppCompatActivity {
     Context mContext;
@@ -46,6 +60,12 @@ public class TripOverviewActivity extends AppCompatActivity {
         tripTitleView.setText(String.format(getString(R.string.TripTitle), mTripId));
     }
 
+    @Override
+    public void onResume() {
+        SetupPieChart();
+        super.onResume();
+    }
+
     Button.OnClickListener MapDisplayListener = new Button.OnClickListener() {
         public void onClick(View v) {
             Intent intent = new Intent(mContext, MapDisplayActivity.class);
@@ -61,4 +81,76 @@ public class TripOverviewActivity extends AppCompatActivity {
             startActivity(intent);
         }
     };
+
+    private void SetupPieChart() {
+        //TODO: Get proper data
+        //Define data
+        List<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(70, 0));
+        entries.add(new Entry(20, 0));
+        entries.add(new Entry(15, 0));
+        entries.add(new Entry(10, 0));
+        entries.add(new Entry(10, 0));
+        entries.add(new Entry(5, 0));
+
+        //Define labels for legend
+        ArrayList<String> labels = new ArrayList<>();
+        labels.add(getString(R.string.SpeedMetricName));
+        labels.add(getString(R.string.AccelerationMetricName));
+        labels.add(getString(R.string.BrakeMetricName));
+        labels.add(getString(R.string.JerkMetricName));
+        labels.add(getString(R.string.RoadTypeMetricName));
+        labels.add(getString(R.string.CriticalTimeMetricName));
+
+        //Create dataset from entries and style it
+        PieDataSet dataSet = new PieDataSet(entries, "");
+        dataSet.setValueTextSize(18);
+        dataSet.setColors(GetGraphColorArray());
+
+        //Combine the dataSet with labels and style it
+        PieData data = new PieData(labels, dataSet);
+        data.setValueFormatter(new PieValueFormatter());
+
+        //Style the PieChart itself
+        PieChart pieChartView = (PieChart) findViewById(R.id.PieChartView);
+        pieChartView.animateY(2000, Easing.EasingOption.EaseInQuad);
+        pieChartView.setDrawHoleEnabled(false);
+        pieChartView.setDrawSliceText(false);
+        pieChartView.setUsePercentValues(true);
+        pieChartView.setDescription("");
+        pieChartView.setDescriptionTextSize(18);
+
+        //Style the legend of the PieChart
+        Legend legend = pieChartView.getLegend();
+        legend.setPosition(Legend.LegendPosition.RIGHT_OF_CHART_CENTER);
+        legend.setTextSize(18);
+        legend.setFormSize(18);
+
+        //Create the PieChart
+        pieChartView.setData(data);
+    }
+
+    public class PieValueFormatter implements ValueFormatter {
+        private DecimalFormat mFormat;
+
+        public PieValueFormatter() {
+            mFormat = new DecimalFormat("##0");
+        }
+
+        @Override
+        public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+            return mFormat.format(value) + "%";
+        }
+    }
+
+    private int[] GetGraphColorArray() {
+        int[] graphColors = new int[6];
+        graphColors[0] = ContextCompat.getColor(this, R.color.graphColorRed);
+        graphColors[1] = ContextCompat.getColor(this, R.color.graphColorYellow);
+        graphColors[2] = ContextCompat.getColor(this, R.color.graphColorPurple);
+        graphColors[3] = ContextCompat.getColor(this, R.color.graphColorGreen);
+        graphColors[4] = ContextCompat.getColor(this, R.color.graphColorTeal);
+        graphColors[5] = ContextCompat.getColor(this, R.color.graphColorBlue);
+        return graphColors;
+    }
 }
