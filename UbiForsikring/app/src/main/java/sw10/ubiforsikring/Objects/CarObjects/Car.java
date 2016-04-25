8007068a46fcb7c701587ebbe24a2970a847b368
Johan Leth Gregersen <johangregersen@live.dk>
2016-04-25 16:23:32 +0200

    Car added.

---
+++ UbiForsikring/app/src/main/java/sw10/ubiforsikring/Objects/CarObjects/Car.java
@@ -0,0 +1,29 @@
+package sw10.ubiforsikring.Objects.CarObjects;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import sw10.ubiforsikring.Helpers.DateObjectHelper;
+
+public class Car {
+    public int CarId;
+    public long IMEI;
+    public String Username;
+
+
+
+    public Car(JSONObject obj) {
+        try {
+            this.CarId = obj.getInt("carid");
+            this.IMEI = obj.getLong("imei");
+            this.Username = obj.isNull("username") ? "" : obj.getString("username");
+
+        } catch (Exception e){
+            Log.e("Debug", "Trip JsonObject Constructor: ", e);
+        }
+    }
+}
